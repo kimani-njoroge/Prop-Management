5feb75941bbb2c69d23278c6f1dcb7f745af61d7
kimani-njoroge <kimani064@gmail.com>
2018-10-28 16:30:05 +0300

    create base template

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,14 +1,5 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.base')
 
-    <title>{{config('app.name', 'Proposal Management')}}</title>
-
-</head>
-<body>
-    <h1>welcome to prop management</h1>
-</body>
-</html>
+@section('content')
+<h1>{{$title}}</h1>
+@endsection
