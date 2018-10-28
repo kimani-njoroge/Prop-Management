f9f502b49ecd6e7337e0174516cda556a4c39761
kimani-njoroge <kimani064@gmail.com>
2018-10-28 23:46:42 +0300

    display information of individual proposal

---
+++ resources/views/proposals/show.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.base')
+@section('content')
+    <a href="/proposals" class="btn btn-defsult">Go Back</a>
+    <h1>{{$proposal->proposaltitle}}</h1>
+
+    <div>
+        {{$proposal->proposal}}
+    </div>
+    <hr>
+    <small>Prepared on {{$proposal->created_at}}</small>
+@endsection
